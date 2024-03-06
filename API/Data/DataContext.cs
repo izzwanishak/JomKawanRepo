620af2dc852144f7a3ef9b0b9a289d1d05adc871
izzwanishak <izzwanishak@gmail.com>
2024-03-06 19:59:00 +0800

    Init API Skeleton

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+//entity name: AppUser
+//table name: Users
+    public DbSet<AppUser> Users { get; set; }
+}
